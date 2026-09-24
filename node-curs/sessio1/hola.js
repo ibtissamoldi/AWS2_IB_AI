e5485f78422c7e6a783b88d06bebb703ab579609
ibtissamoldi <ibtissamoldi@gmail.com>
2026-09-24 17:38:53 +0200

    primer sessio js

---
+++ node-curs/sessio1/hola.js
@@ -0,0 +1,3 @@
+console.log("Hola des de Node.js!");
+console.log("Versio: ",process.version);
+console.log("Carpeta: ",process.cwd());
---
+++ node-curs/sessio1/saluda.js
@@ -0,0 +1,5 @@
+const nom = process.argv[2] || 'desconegut';
+const edat = process.argv[3] || '0';
+
+
+console.log(`Bon dia, ${nom}! L'any que ve tindras ${edat} anys`);
